c99f07c17a3ba846e1bb293bf324def7d8514451
Bryan Kelly <bkelly@detroitlabs.com>
2017-01-06 16:19:54 -0500

    Removed all but one sample unit tests.

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -1,30 +1,11 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.junit.Test;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import org.junit.*;
-
-import play.mvc.*;
-import play.test.*;
-import play.data.DynamicForm;
-import play.data.validation.ValidationError;
-import play.data.validation.Constraints.RequiredValidator;
-import play.i18n.Lang;
-import play.libs.F;
-import play.libs.F.*;
-import play.twirl.api.Content;
-
-import static play.test.Helpers.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 
 /**
- *
  * Simple (JUnit) tests that can call all parts of a play app.
  * If you are interested in mocking a whole application, see the wiki for more details.
- *
  */
 public class ApplicationTest {
 
@@ -32,13 +13,6 @@ public class ApplicationTest {
     public void simpleCheck() {
         int a = 1 + 1;
         assertEquals(2, a);
-    }
-
-    @Test
-    public void renderTemplate() {
-        Content html = views.html.index.render("Your new application is ready.");
-        assertEquals("text/html", html.contentType());
-        assertTrue(html.body().contains("Your new application is ready."));
     }
 
 
